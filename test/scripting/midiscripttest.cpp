32775a2b1eda1a9d41ddc5f8a06e541b67eed410
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2026-02-16 21:26:02 +0100

    use `BOOST_CHECK_EQUAL` for assertions

--- test/scripting/midiscripttest.cpp
+++ test/scripting/midiscripttest.cpp
@@ -7,15 +7,13 @@ BOOST_AUTO_TEST_CASE (NoteOn)
 {
     LuaFixture fix;
     sol::state_view lua (fix.luaState());
-    lua.script("midi = require('el.midi')");
+    lua.script ("midi = require('el.midi')");
 
-    // Get the packed integer from Lua
-    int64_t packed = lua.script("return midi.noteon(1, 60, 100)");
+    int64_t packed = lua.script ("return midi.noteon(1, 60, 100)");
 
-    // Compare against JUCE
-    auto expected = juce::MidiMessage::noteOn(1, 60, (uint8_t)100);
-    BOOST_REQUIRE(uint8_t(packed)       == expected.getRawData()[0]);
-    BOOST_REQUIRE(uint8_t(packed >> 8)  == expected.getRawData()[1]);
-    BOOST_REQUIRE(uint8_t(packed >> 16) == expected.getRawData()[2]);
+    auto expected = juce::MidiMessage::noteOn (1, 60, (uint8_t) 100);
+    BOOST_CHECK_EQUAL (uint8_t (packed), expected.getRawData()[0]);
+    BOOST_CHECK_EQUAL (uint8_t (packed >> 8), expected.getRawData()[1]);
+    BOOST_CHECK_EQUAL (uint8_t (packed >> 16), expected.getRawData()[2]);
 }
 BOOST_AUTO_TEST_SUITE_END()
