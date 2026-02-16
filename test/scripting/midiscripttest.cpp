dc2e24293c72c0941244f16bec19f8d0e4a747d1
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2026-02-16 22:15:05 +0100

    Extend `BOOST_DATA_TEST_CASE` with structured Lua MIDI test cases

--- test/scripting/midiscripttest.cpp
+++ test/scripting/midiscripttest.cpp
@@ -5,19 +5,41 @@
 BOOST_AUTO_TEST_SUITE (MidiScriptTests)
 
 namespace bdata = boost::unit_test::data;
+using JuceCounterpart = std::function<juce::MidiMessage()>;
+
+struct LuaMidiTestCase {
+    std::string luaMethodCall;
+    JuceCounterpart juceCounterpart;
+
+    friend std::ostream& operator<<(std::ostream& os, const LuaMidiTestCase& testCase) {
+        os << "{ luaMethodCall: \"" << testCase.luaMethodCall << "\", ... } ";
+        return os;
+    }
+};
+
 BOOST_DATA_TEST_CASE (
-    NoteOn,
-    bdata::make ({ "return midi.noteon(1, 60, 100)" })
-        ^ bdata::make ({ []() -> juce::MidiMessage { return juce::MidiMessage::noteOn (1, 60, (uint8_t) 100u); } }),
-    script, juceCounterpart)
+    luaSimpleBindigs,
+    bdata::make ({
+        LuaMidiTestCase {
+            "return midi.noteon(1, 60, 100)",
+            []() -> juce::MidiMessage { return juce::MidiMessage::noteOn (1, 60, (uint8_t) 100u); }
+        },
+
+        LuaMidiTestCase {
+            "return midi.noteoff(1, 60)",
+            []() -> juce::MidiMessage { return juce::MidiMessage::noteOff (1, 60); }
+        },
+
+    }),
+    testData)
 {
     LuaFixture fix;
     sol::state_view lua (fix.luaState());
     lua.script ("midi = require('el.midi')");
 
-    int64_t packed = lua.script (script);
+    int64_t packed = lua.script (testData.luaMethodCall);
 
-    auto expected = juceCounterpart();
+    auto expected = testData.juceCounterpart();
     BOOST_CHECK_EQUAL (uint8_t (packed), expected.getRawData()[0]);
     BOOST_CHECK_EQUAL (uint8_t (packed >> 8), expected.getRawData()[1]);
     BOOST_CHECK_EQUAL (uint8_t (packed >> 16), expected.getRawData()[2]);
